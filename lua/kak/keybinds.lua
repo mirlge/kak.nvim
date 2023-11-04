413d64d5887b0a7b1e73bdc6a49a22b79873b80e
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-11-04 15:10:48 +0100

    feat(keybinds): Reuse table with hjkl and ai keybinds, add word and fFtT keybinds to <A- keybinds list

--- lua/kak/keybinds.lua
+++ lua/kak/keybinds.lua
@@ -1,12 +1,15 @@
-for _, key in ipairs({ "h", "j", "k", "l", "i", "a" }) do
+local movement_ai = { "h", "j", "k", "l", "i", "a" }
+
+for _, key in ipairs(movement_ai) do
   vim.keymap.set({ "x" }, key, "<esc>" .. key)
 end
 
 local word_fFtT = { "w", "e", "b", "W", "E", "B", "f", "F", "t", "T" }
 
-for _, key in ipairs({ "h", "j", "k", "l", "i", "a" }) do
-  vim.keymap.set({ "x" }, "<A-" .. key .. ">", function() vim.cmd("norm! " .. key) end)
-  vim.keymap.set({ "n" }, "<A-" .. key .. ">", function() vim.cmd("norm! v" .. key) end)
+for _, keytbl in ipairs({ movement_ai, word_fFtT }) do
+  for _, key in ipairs(keytbl) do
+    vim.keymap.set({ "n", "x" }, "<A-" .. key .. ">", function() vim.cmd("norm! v" .. key) end)
+  end
 end
 
 for _, key in ipairs(word_fFtT) do
