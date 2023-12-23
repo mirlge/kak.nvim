8b70d8ebfd4bed840911b15c309824acce2162d6
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-23 15:01:25 +0100

    fix(keybinds): Add working keybinds for around and inside Visual mode motions

--- lua/kak/keybinds.lua
+++ lua/kak/keybinds.lua
@@ -1,37 +1,47 @@
 local M = {}
 
 function M.setup(opts)
-  local opts = opts or {}
+	local opts = opts or {}
 
-  if not opts.disable_full then
-    local movement_ai = { "h", "j", "k", "l", "i", "a" }
-    local word_fFtT = { "w", "e", "b", "W", "E", "B", "ge", "gE", "f", "F", "t", "T" }
+	if not opts.disable_full then
+		local movement_ai = { "h", "j", "k", "l", "i", "a" }
+		local word_fFtT = { "w", "e", "b", "W", "E", "B", "ge", "gE", "f", "F", "t", "T" }
+		local around_inside = { "a", "i" }
 
-    for _, keytbl in ipairs({ movement_ai, word_fFtT }) do
-      for _, key in ipairs(keytbl) do
-        vim.keymap.set("x", "<A-" .. key .. ">", function()
-          vim.cmd("norm! " .. key)
-        end)
-      end
-    end
+		for _, keytbl in ipairs({ movement_ai, word_fFtT }) do
+			for _, key in ipairs(keytbl) do
+				vim.keymap.set("x", "<A-" .. key .. ">", function()
+					vim.cmd("norm! " .. key)
+				end)
+			end
+		end
 
-    for _, key in ipairs(movement_ai) do
-      vim.keymap.set("x", key, "<esc>" .. key)
-      vim.keymap.set("n", "<A-" .. key .. ">", function()
-        vim.cmd("norm! v" .. key)
-      end)
-    end
+		for _, key in ipairs(movement_ai) do
+			vim.keymap.set("x", key, "<esc>" .. key)
+			vim.keymap.set("n", "<A-" .. key .. ">", function()
+				vim.cmd("norm! v" .. key)
+			end)
+		end
 
-    for _, key in ipairs(word_fFtT) do
-      vim.keymap.set({ "n", "x" }, key, "<esc>v" .. key)
-    end
-  end
+		for _, key in ipairs(word_fFtT) do
+			vim.keymap.set({ "n", "x" }, key, "<esc>v" .. key)
+		end
 
-  for _, key in ipairs({ "d", "c", "y" }) do
-    vim.keymap.set({ "n" }, key, "v" .. key)
-  end
-  vim.keymap.set({ "n", "x" }, "x", "V")
-  --vim.keymap.set({ "n", "x" }, "%", "gg<esc>VG")
+		for _, key in ipairs(around_inside) do
+			vim.keymap.set("n", "<A-" .. key .. ">", function()
+				vim.cmd("norm! v" .. key .. vim.fn.input("v" .. key))
+			end)
+			vim.keymap.set("x", "<A-" .. key .. ">", function()
+				vim.cmd("norm! " .. key .. vim.fn.input("v" .. key))
+			end)
+		end
+	end
+
+	for _, key in ipairs({ "d", "c", "y" }) do
+		vim.keymap.set({ "n" }, key, "v" .. key)
+	end
+	vim.keymap.set({ "n", "x" }, "x", "V")
+	--vim.keymap.set({ "n", "x" }, "%", "gg<esc>VG")
 end
 
 return M
