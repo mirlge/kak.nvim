5ced02f2adbab1db10d593850524a932ee1d59da
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-26 12:55:00 +0100

    refactor(keybinds): Replace single-char input()s with getcharstr()s
    Doesn't ask for input on the command line, and also doesn't accept more
    than one keypress

--- lua/kak/keybinds.lua
+++ lua/kak/keybinds.lua
@@ -103,17 +103,17 @@ function M.setup(opts)
     for _, key in ipairs(around_inside) do
       local key_wrapped = "<A-" .. key .. ">"
       vim.keymap.set("n", key_wrapped, function()
-        vim.cmd("norm! v" .. key .. vim.fn.input(key_wrapped))
+        vim.cmd("norm! v" .. key .. vim.fn.getcharstr())
       end)
       vim.keymap.set("x", key_wrapped, function()
-        vim.cmd("norm! " .. vim.keycode("<Esc>") .. "v" .. key .. vim.fn.input(key_wrapped))
+        vim.cmd("norm! " .. vim.keycode("<Esc>") .. "v" .. key .. vim.fn.getcharstr())
       end)
 
       vim.keymap.set("x", key, "<Esc>" .. key)
     end
     for _, key in ipairs(ft) do
       vim.keymap.set("n", key, function()
-        local char = vim.fn.input(key)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
@@ -127,7 +127,7 @@ function M.setup(opts)
         vim.cmd("norm! " .. pre_count .. "v" .. key .. char)
       end)
       vim.keymap.set("x", key, function()
-        local char = vim.fn.input(key)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
@@ -144,7 +144,7 @@ function M.setup(opts)
       local upper_key = string.upper(key)
 
       vim.keymap.set("n", upper_key, function()
-        local char = vim.fn.input(key)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
@@ -154,7 +154,7 @@ function M.setup(opts)
         vim.cmd("norm! v" .. count .. key .. char)
       end)
       vim.keymap.set("x", upper_key, function()
-        local char = vim.fn.input(key)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
@@ -166,7 +166,7 @@ function M.setup(opts)
 
       local key_wrapped = "<A-" .. key .. ">"
       vim.keymap.set("n", key_wrapped, function()
-        local char = vim.fn.input(key_wrapped)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
@@ -176,7 +176,7 @@ function M.setup(opts)
         vim.cmd("norm! v" .. count .. upper_key .. char)
       end)
       vim.keymap.set("x", key_wrapped, function()
-        local char = vim.fn.input(key_wrapped)
+        local char = vim.fn.getcharstr()
 
         local count = vim.v.count
         if count < 1 then
