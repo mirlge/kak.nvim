1b559f86ed184925de6145849bf13bab535939a9
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-27 10:19:33 +0100

    feat(defaults): Set `full` to a table containing the individual options and their descriptions

--- lua/kak/defaults.lua
+++ lua/kak/defaults.lua
@@ -1,3 +1,6 @@
 return {
-  full = true, -- enable all keybinds except Normal mode c, d, x, y
+  full = {
+    enable = true,     -- enable all keybinds except Normal mode c, d, x and y, same as setting full to true
+    visual_only = false, -- makes you be only in Visual mode
+  },
 }
