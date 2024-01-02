18013710cd9954449a616a4c4204fe2d0a1b7350
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-02 10:46:31 +0100

    fix(defaults): Make comment for `full` option be correct

--- lua/kak/defaults.lua
+++ lua/kak/defaults.lua
@@ -1,4 +1,4 @@
 return {
-  full = true,        -- enable all keybinds except Normal mode c, d, x and y, same as setting full to true
   visual_only = false, -- makes you be only in Visual mode
+  full = true, -- if set to false, it disables all keybinds except Normal mode c, d, x and y
 }
